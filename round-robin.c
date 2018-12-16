9c5bee7f35c3426deae4bc3e66b79f84684193d8
ma_1750 <mc.ma1750@gmail.com>
2018-12-16 21:06:00 +0900

    fix bug and replace braces and spaces

--- round-robin.c
+++ round-robin.c
@@ -1,114 +1,80 @@
 #include <stdio.h>
 #include "round-robin.h"
 
-void Enqueue(task*);
-task* Dequeue();
-void print_result(task*, int);
-
 int main()
 {
-	task task_list[TASK_COUNT];
+	task_t task_list[TASK_COUNT];
 	queuehead = queuetail = 0;
 	int timer = 0;
 	int finished_task_number = 0;
 	int num_of_tasks;
 	int step;
 
-	//input
+	/*input*/
 	printf("input : Task_Number\n");
 	scanf("%d", &num_of_tasks);
-	printf("input : Step_Number\n");
-	scanf("%d", &step);
 	printf("input : Task_Name Arrival_Time Cost\n");
-	for(int i = 0; i < num_of_tasks; ++i)
-	{
-		scanf(" %s %d %d", task_list[i].name, &task_list[i].arrival_time, &task_list[i].cost);
+	for (int i = 0; i < num_of_tasks; ++i) {
+		scanf(" %s %d %d",
+			task_list[i].name, &task_list[i].arrival_time, &task_list[i].cost);
 		task_list[i].progress = 0;
 		task_list[i].status = 0;
 	}
+	printf("input : Step_Number\n");
+	scanf("%d", &step);
 
-	//sort task_list arrival order
-	//bubble sort is not so good
-	for(int i = 0; i < num_of_tasks; ++i)
-		for(int j = 0; j < num_of_tasks; ++j)
-		{
-			if (task_list[i].arrival_time < task_list[j].arrival_time)
-			{
-				task tmp = task_list[i];
+	/*sort task_list arrival order*/
+	for (int i = 0; i < num_of_tasks; ++i)
+		for (int j = 0; j < num_of_tasks; ++j) {
+			if (task_list[i].arrival_time < task_list[j].arrival_time) {
+				task_t tmp = task_list[i];
 				task_list[i] = task_list[j];
 				task_list[j] = tmp;
 			}
 		}
 
-	Enqueue(&task_list[0]);
-	task_list[0].status = 1;
-
-	//main process
+	/*main process*/
 	printf("\nstart\n");
-	while(finished_task_number != num_of_tasks)
-	{
+	while (finished_task_number != num_of_tasks) {
 		finished_task_number = 0;
-		task *current_job = Dequeue();
-		if(current_job == NULL)
-		{
+		fetch_new_task(task_list, num_of_tasks, timer);
+		task_t *current_job = dequeue();
+		if (current_job == NULL) {
 			++timer;
-			for (int i = 0; i < num_of_tasks; ++i)
-			{
+			for (int i = 0; i < num_of_tasks; ++i) {
 				if(task_list[i].status == 2)
 					++finished_task_number;
 			}
-			if(finished_task_number != num_of_tasks)
-			{
-				for(int i = 0; i < num_of_tasks; ++i)
-				{
-					if(task_list[i].arrival_time <= timer && task_list[i].status == 0)
-					{
-						Enqueue(&task_list[i]);
-						task_list[i].status = 1;
-					}
-				}
+			if (finished_task_number != num_of_tasks) {
+				if (timer)
+					printf("%2d skip\n", timer);
 				continue;
-			}
-			else
-			{
-				printf("Eroor\n");
-				return -1;
+			} else {
+				break;
 			}
 		}
 
 		int counter = timer + step;
-		if(current_job->cost - current_job->progress <= step)
+		if (current_job->cost - current_job->progress <= step)
 			counter = timer + current_job->cost - current_job->progress;
-		while(timer < counter)
-		{
+		while (timer < counter) {
 			++timer;
 			++current_job->progress;
-			if(current_job->cost == current_job->progress)
-			{
+			if (current_job->cost == current_job->progress) {
 				current_job->status = 2;
 				printf("%2d Task:%s is finish\n", timer, current_job->name);
 				current_job->fin_time = timer;
-			}
-			else
-			{
+			} else {
 				printf("%2d Task:%s\n", timer, current_job->name);
 			}
-			for(int i = 0; i < num_of_tasks; ++i)
-			{
-				if(task_list[i].arrival_time == timer && task_list[i].status == 0)
-				{
-					Enqueue(&task_list[i]);
-					task_list[i].status = 1;
-				}
-			}
+			fetch_new_task(task_list, num_of_tasks, timer);
 		}
-		for (int i = 0; i < num_of_tasks; ++i)
-		{
+		for (int i = 0; i < num_of_tasks; ++i) {
 			if(task_list[i].status == 2)
 				++finished_task_number;
 		}
 		if(current_job->status == 1)
-			Enqueue(current_job);
+			enqueue(current_job);
 	}
 	printf("end\n\n");
 	print_result(task_list, num_of_tasks);
--- round-robin.h
+++ round-robin.h
@@ -3,44 +3,60 @@
 static int queuehead;
 static int queuetail;
 
-typedef struct{
-    char name[10];
-    int arrival_time;
-    int cost;
-    int progress;
-    int fin_time;
-    int status;/*0:not into queue 1:in queue 2:finished execute*/
-} task;
+typedef struct {
+	char name[10];
+	int arrival_time;
+	int cost;
+	int progress;
+	int fin_time;
+	int status;
+	/*
+	 *0:not into queue
+	 *1:in queue
+	 *2:finished execute
+	 */
+} task_t;
 
-static task* queue[Q_SIZE];
+static task_t* queue[Q_SIZE];
 
-void Enqueue(task *x)
+void enqueue(task_t *x)
 {
-    queuetail = (queuetail+1)%Q_SIZE;
-    queue[queuetail] = x;
-    if(queuehead == queuetail)
-    {
-        printf("Eroor: Overflow\n");
-        return;
-    }
+	queuetail = (queuetail+1) % Q_SIZE;
+	queue[queuetail] = x;
+	if (queuehead == queuetail) {
+		printf("Eroor: Overflow\n");
+		return;
+	}
 }
 
-task* Dequeue()
+task_t* dequeue()
 {
-    if(queuehead == queuetail)
-        return NULL;
-    queuehead = (queuehead+1)%Q_SIZE;
-    return queue[queuehead];
+	if (queuehead == queuetail)
+		return NULL;
+	queuehead = (queuehead + 1) % Q_SIZE;
+	return queue[queuehead];
 }
 
-void print_result(task *task_list, int tasks)
+void print_result(task_t *task_list, int tasks)
 {
-    int sum = 0;
-    printf("Task_Name|Arrival_Time|Finish_Time|\n");
-    for(int i = 0; i < tasks; ++i)
-    {
-        printf("\t%s|\t%d|\t%d|\n", task_list[i].name, task_list[i].arrival_time, task_list[i].fin_time);
-        sum += (task_list[i].fin_time - task_list[i].arrival_time);
-    }
-    printf("\nAverage_Process_Time = %.2f\n", (double)sum/tasks);
+	int sum = 0;
+	printf("Task_Name|Arrival_Time|Finish_Time|\n");
+	for (int i = 0; i < tasks; ++i) {
+		printf("%9s|%12d|%11d|\n",
+			task_list[i].name, task_list[i].arrival_time,
+			task_list[i].fin_time);
+		sum += task_list[i].fin_time - task_list[i].arrival_time;
+	}
+	printf("\nAverage_Process_Time = %.2f\n", (double)sum/tasks);
+}
+
+void fetch_new_task(task_t *task_list, int num_of_tasks, int time)
+{
+	for (int i = 0; i < num_of_tasks; ++i) {
+		if(task_list[i].arrival_time <= time
+				&& !task_list[i].status) {
+			enqueue(&task_list[i]);
+			task_list[i].status = 1;
+		}
+	}
 }
